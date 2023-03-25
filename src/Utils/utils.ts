dc46c3b033f41e760fecaa5adc19b7620827fc2f
Davidjayan <davidjayandj@gmail.com>
2023-03-25 10:16:34 +0530

    [Davidjayan] church offering t pdf added

--- src/Utils/utils.ts
+++ src/Utils/utils.ts
@@ -1,4 +1,4 @@
-import moment from "moment"
+import moment from 'moment'
 
 export const returnTemplate = (status: number, message: any, res: any) => {
   if (status == 1) {
@@ -58,12 +58,18 @@ export const buildHtml = (
 ) => {
   let headerItems = ''
   let totalsitems = '<th>Totals</th>'
+  let churchOffering = '<td colspan="7"></td><th colspan="2">Church Offerings</th>'
+  let grandTotal = '<td colspan="7"></td><th colspan="2">Grand Total</th>'
 
   totalsColumnNames.forEach((col) => {
-    if (col != 'ChurchOffering' && col != 'GrandTotal') {
-      let total = totals.reduce((prev: any, curr: any) => {
-        return prev + curr[col]
-      }, 0)
+    let total = totals.reduce((prev: any, curr: any) => {
+      return prev + curr[col]
+    }, 0)
+    if (col == 'ChurchOffering') {
+      churchOffering = churchOffering + '<td colspan="2">' + total + '</td>'
+    } else if (col == 'GrandTotal') {
+      grandTotal = grandTotal + '<td colspan="2">' + total + '</td>'
+    } else {
       totalsitems = totalsitems + '<td>' + total + ' </td>'
     }
   })
@@ -73,6 +79,7 @@ export const buildHtml = (
   })
   let tableOnehtmlBodyTd = ''
   let tableTwoHtmlBody = ''
+
   data.forEach((element: any) => {
     let dataRow = ''
     offeringTypes.forEach((value) => {
@@ -96,7 +103,13 @@ export const buildHtml = (
         value * total
       } </td></tr>`
   })
-  const tableOnehtmlHeaderTh = `<thead><tr><th colspan='11'>JEHOVAH NISSI AG CHURCH ACCOUNTS REPORT</th></tr><tr><th colspan='11'>${toDate ? `REPORT FROM ${moment(fromDate).format('DD/MM/YYYY')} TO ${moment(toDate).format('DD/MM/YYYY')}`:`REPORT ON ${moment(fromDate).format('DD/MM/YYYY')}`} </th>
+  const tableOnehtmlHeaderTh = `<thead><tr><th colspan='11'>JEHOVAH NISSI AG CHURCH ACCOUNTS REPORT</th></tr><tr><th colspan='11'>${
+    toDate
+      ? `REPORT FROM ${moment(fromDate).format('DD/MM/YYYY')} TO ${moment(
+          toDate,
+        ).format('DD/MM/YYYY')}`
+      : `REPORT ON ${moment(fromDate).format('DD/MM/YYYY')}`
+  } </th>
      </tr><tr>
       ${headerItems}
      </tr></thead>`
@@ -109,7 +122,7 @@ export const buildHtml = (
       </tr>
     </thead>`
 
-  const tableOne = `<table id="t1">${tableOnehtmlHeaderTh}<tbody>${tableOnehtmlBodyTd}<tr>${totalsitems}</tr></tbody></table>`
+  const tableOne = `<table id="t1">${tableOnehtmlHeaderTh}<tbody>${tableOnehtmlBodyTd}<tr>${totalsitems}</tr><tr>${churchOffering}</tr><tr>${grandTotal}</tr></tbody></table>`
   const tableTwo = `<table id="t2">${tableTwohtmlHeaderTh}<tbody>${tableTwoHtmlBody}</tbody></table>`
 
   return `<html><style>table{margin-top:4px;} table,th{border:1px solid black; border-collapse: collapse;padding:5px;} #t1 > tbody > tr > td {text-align:center;border:1px solid black;padding:5px;}#t2 > tbody > tr > td {padding:5px;}</style>${tableOne}${tableTwo}</html>`
